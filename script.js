d49510f5268b49824691d5a319d0dd24f0fc172f
Ayu914545sh <168348479+Ayu914545sh@users.noreply.github.com>
2024-12-11 10:32:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add scrolling effect for navbar background
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
